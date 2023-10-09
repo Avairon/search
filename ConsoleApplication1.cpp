6be096aba5e370be18d601bb750bf33b3cf76be0
Avairon <75504999+Avairon@users.noreply.github.com>
2023-10-09 11:02:59 +0000

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+void first(int* arr, int arg) {
+    bool result = false;
+
+    int arg2 = sizeof(arr) / sizeof(int);
+    int lenght = pow(arg2, 0.5);
+
+    for (int y = 0; y < lenght; y++) {
+        for (int x = 0; x < lenght; x++) {
+            if (array(x, y) == arg) { cout << x << " " << y << "\n"; result = true; break; }
+        }
+    }
+    if (result == false) cout << "This number is missing from the array";
+    return;
+}
+
+int main()
+{
+    int arr[3][3] = {{1, 2, 3}, 
+                     {4, 5, 6}, 
+                     {7, 8, 9}};
+
+    int input;
+
+    cout << "Input the search number\n";
+    cin >> input;
+
+    return 0;
+}
